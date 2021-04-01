cfc1d793888bb54fb50cdbb8a6c3b565590028c0
RicardoArrobo <ricardoarrobo@gmail.com>
2021-04-01 11:38:40 -0500

    add: function examples & constructor

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -3,16 +3,41 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract BankAccount{
     mapping (address => uint) balanceRecieved;
+    
+    address payable owner;
+    
+    // Only calls on contract deployment
+    constructor(){
+        owner = payable(msg.sender);
+    }
+    // Can access to store vars but not change it's state
+    function getOwner() public view returns(address){
+        return owner;
+    }
+    // Can't access to store vars at all.
+    function convertWeiToEth(uint _amountInWei) public pure returns (uint){
+        return _amountInWei / 1 ether;
+    }
+    
+     // Fallback function for receive ETH when the SC dont have any function to transfer eth.
+   receive() external payable { 
+       receiveMoney();
+   }
+    
+    function destroySmartContract () public{
+        require(msg.sender == owner, "You are not the owner");
+        selfdestruct(owner);
+    }
 
     function getBalance() public view returns(uint){
         return address(this).balance;
     }
 
-    function sendMoney() public payable{
+    function receiveMoney() public payable{
         balanceRecieved[msg.sender] += msg.value;
     }
 
-    function widthdrawAllMoney(address payable _to, uint _amount) public {
+    function widthdrawMoney(address payable _to, uint _amount) public {
         uint currentBalance = balanceRecieved[msg.sender];
         require(_amount <= currentBalance, "not enough funds.");
         balanceRecieved[msg.sender] -= _amount;
